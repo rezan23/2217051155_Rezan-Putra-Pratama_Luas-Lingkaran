bf5634a3706ab722c01f5ffb5c77022a80ee516d
Rezan <112567784+rezan23@users.noreply.github.com>
2022-09-09 09:36:23 +0700

    Add files via upload

---
+++ 2217051155_Rezan Putra Pratama_Luas Lingkaran.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	float r, luas;
+	
+cout << "Masukkan jari-jari lingkaran : ";
+cin>> r;
+luas = 3.14 *r*r;
+cout << "Hasil Luas Lingkaran : "<< luas << endl;
+
+return 0;
+}
